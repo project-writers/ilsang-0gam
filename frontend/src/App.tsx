98c62633dd3468c87c30a9238681cd9715703d10
wis <kogoome@gmail.com>
2023-07-01 20:45:48 +0900

    add : start page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { GoogleLogoSvg } from './component/GoogleLogoSVG.tsx'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
 				<div className="absolute bottom-0 left-0 w-full h-[276px] bg-white rounded-t-[40px] ">
 					<button className="relative top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[90%] h-[60px] border-2 rounded-[80px] border-[#BABABA] text-center text-xl font-bold items-center ">
 						로그인
-						<GoogleLogoSvg className="absolute top-1/2 translate-x-14 -translate-y-1/2" />
+						<GoogleLogoSvg className="absolute top-1/2 translate-x-16 -translate-y-1/2" />
 					</button>
 				</div>
 			</div>
