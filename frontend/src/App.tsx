e3a992222687e15029791da8cec38de055b100f2
wis <kogoome@gmail.com>
2023-07-01 20:54:27 +0900

    fix : start page 분리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
-import { GoogleLogoSvg } from './component/GoogleLogoSVG.tsx'
+import { Start } from './page/Start.tsx'
 
 function App() {
 	return (
 		<>
-			<div className="bg-[#D0956A] w-screen h-screen relative">
-				<div className="absolute bottom-0 left-0 w-full h-[276px] bg-white rounded-t-[40px] ">
-					<button className="relative top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[90%] h-[60px] border-2 rounded-[80px] border-[#BABABA] text-center text-xl font-bold items-center ">
-						로그인
-						<GoogleLogoSvg className="absolute top-1/2 translate-x-16 -translate-y-1/2" />
-					</button>
-				</div>
-			</div>
+			<Start />
 		</>
 	)
 }
