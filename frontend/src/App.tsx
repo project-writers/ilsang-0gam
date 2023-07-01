7daa9967f0f57e4b9167d4fb47c4ad41c230e284
wis <kogoome@gmail.com>
2023-07-01 20:06:26 +0900

    add : startpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+	return (
+		<>
+			<div className="bg-[#D0956A] w-screen h-screen relative">
+				<div className="absolute bottom-0 left-0 w-full h-[276px] bg-white rounded-t-[40px] p- flex flex-col justify-center">
+					<div className="w-full h-[60px] border-2 rounded-full border-slate-400 text-center">
+						로그인
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
