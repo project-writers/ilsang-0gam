13d35918e324a8ef26ba9c72324ea27e2a5d278e
wis <kogoome@gmail.com>
2023-07-05 20:31:43 +0900

    fix : signup 통합

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,23 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Start } from "./page/Start.tsx";
-import { SignUp } from "./page/SignUp";
-import { MainPage } from "./page/Main";
-import { Watch } from "./page/Watch";
-import { Write } from "./page/Write";
-import { MyPage } from "./page/MyPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Start } from './page/Start.tsx'
+import { SignUp } from './page/SignUp'
+import { MainPage } from './page/Main'
+import { Watch } from './page/Watch'
+import { Write } from './page/Write'
+import { MyPage } from './page/MyPage'
 
 export function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Start />}></Route>
-        <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/main" element={<MainPage />}>
-          <Route path="/main/:id" element={<Watch />} />
-        </Route>
-        <Route path="/write" element={<Write />}></Route>
-        <Route path="/mypage" element={<MyPage />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Start />}></Route>
+				<Route path="/signup" element={<SignUp />}></Route>
+				<Route path="/main" element={<MainPage />}>
+					<Route path="/main/:id" element={<Watch />} />
+				</Route>
+				<Route path="/write" element={<Write />}></Route>
+				<Route path="/mypage" element={<MyPage />}></Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
--- frontend/src/page/SignUp.tsx
+++ frontend/src/page/SignUp.tsx
@@ -1,3 +1,44 @@
+import { KeyboardEvent, useState } from 'react'
+import { Link } from 'react-router-dom'
+
 export function SignUp() {
-  return <div>SignUP</div>;
+	const [nameState, setNameState] = useState<{
+		res: null | boolean
+		message: string
+	}>({ res: null, message: '' })
+	const keydown = (e: KeyboardEvent) => {
+		if (e.key !== 'Enter') return
+		const value = (e.target as HTMLInputElement).value
+		// nickname validation fetching
+		if (value === '홍길동')
+			setNameState({ res: true, message: '사용이 가능합니다' })
+		else setNameState({ res: false, message: '이미 존재하는 필명입니다' })
+	}
+
+	return (
+		<div className="w-screen h-screen flex flex-col justify-center">
+			{/* input */}
+			<div className="w-full h-full flex flex-col justify-center gap-[17px] px-[5%]">
+				<div className="w-full text-center font-bold text-2xl">
+					당신의 필명은?
+				</div>
+				<input
+					type="text"
+					className="h-[60px] rounded-[80px] text-center text-xl font-bold items-center bg-white border-2 border-[#BABABA]"
+					onKeyDown={keydown}
+				/>
+				<div className={`${nameState.res ? 'text-green-300' : 'text-red-300'}`}>
+					{nameState.message}
+				</div>
+			</div>
+			{/* footer */}
+			<div className="w-full h-[276px] bg-[#707070] rounded-t-[40px] px-[5%]">
+				<Link to="/main">
+					<button className="relative top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-full h-[60px] rounded-[80px] text-center text-xl font-bold items-center bg-white">
+						확인
+					</button>
+				</Link>
+			</div>
+		</div>
+	)
 }
