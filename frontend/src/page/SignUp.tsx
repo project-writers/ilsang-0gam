57b71c3d0f5cb3ed1e041b207ae1e800bb7552e7
wis <kogoome@gmail.com>
2023-07-06 20:27:57 +0900

    merge test

--- frontend/src/page/SignUp.tsx
+++ frontend/src/page/SignUp.tsx
@@ -2,46 +2,22 @@ import { KeyboardEvent, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export function SignUp() {
-	const [nameState, setNameState] = useState<{
+	const [sssssssssss, setNameState] = useState<{
 		res: null | boolean
 		message: string
 	}>({ res: null, message: 'ㅤ' })
 	const ref = useRef<HTMLInputElement>(null)
 	const navigate = useNavigate()
 
-	const keydown = (e: KeyboardEvent) => {
-		if (!ref.current) return
-		// TODO frontend nickname validation
-		// 의논할 사항. 필명 제약조건 얘기해보기. 특수문자, 띄어쓰기. 정규분포
-		// const res = false
-		if (ref.current.value === '홍길동')
-			setNameState({ res: true, message: '사용이 가능합니다' })
-		else setNameState({ res: false, message: '이미 존재하는 필명입니다' })
-
-		if (e.key === 'Enter') registerNickname()
-	}
 	const registerNickname = async () => {
-		if (!nameState.res || !ref.current) return
+		if (!sssssssssss.res || !ref.current) return
 
 		// TODO get email from jwt
 		const email = 'user@email.com'
 		// TODO registration data fetching with backend server
 		const data = { email, nickName: ref.current.value }
 		// todo fetching url
-		// const res: {
-		// 	state: boolean
-		// 	msg: string
-		// } = await fetch('/registerUrl', {
-		// 	method: 'POST',
-		// 	headers: {
-		// 		'Content-Type': 'application/json',
-		// 	},
-		// 	body: JSON.stringify(data),
-		// })
-		// 	.then((res) => res.json())
-		// 	.catch((e) => {
-		// 		return { state: false, msg: `error : ${e}` }
-		// 	})
+
 		const res = { state: Math.random() >= 0.5 }
 
 		if (res.state) navigate('/main')
@@ -61,7 +37,6 @@ export function SignUp() {
 						type="text"
 						maxLength={20}
 						className="absolute top-0 left-0 w-full h-full outline-none text-center text-xl font-bold items-center content-center"
-						onKeyDown={keydown}
 					/>
 					<div className="absolute top-0 right-0 w-32 h-full p-1">
 						<button
@@ -74,12 +49,12 @@ export function SignUp() {
 				</div>
 				<div
 					className={`opacity-0 ${
-						nameState.res
+						sssssssssss.res
 							? 'text-green-300 opacity-100'
 							: 'text-red-300 opacity-100'
 					}`}
 				>
-					{nameState.message}
+					{sssssssssss.message}
 				</div>
 			</div>
 			{/* footer UI 디자인에 문제가 있음. 키보드배열이 하단 버튼화면을 가려서 사용성을 떨어뜨림. 인풋필드 오른쪽에 확인버튼 심는게 나을 듯 */}
