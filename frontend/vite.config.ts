d17555644a29415e95527242b75d30989487a753
wis <kogoome@gmail.com>
2023-07-06 22:53:09 +0900

    fix : 경로 별칭 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: '@component',
+				replacement: path.resolve(__dirname, 'src/component'),
+			},
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+		],
+	},
 })
