66bf20ce1fcaecae4e5af4a42f8af14a1a78a79d
wis <kogoome@gmail.com>
2023-07-07 01:20:21 +0900

    remove : 사용하지 않는 의존성 삭제

--- frontend/src/page/Start.tsx
+++ frontend/src/page/Start.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { GoogleLogoSvg } from '@component/GoogleLogoSVG.tsx'
 import { useNavigate } from 'react-router-dom'
 
