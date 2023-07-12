19e64e227d98537988b87dd7034fb41fc296328b
Nam Min Ki <nam960216@gmail.com>
2023-07-12 13:26:40 +0900

    fix : watch 페이지 라우터 변경

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -8,20 +8,19 @@ import { MyPage } from './page/mypage/MyPage'
 import { useState } from 'react'
 
 export function Router() {
-	const publish = useState('github')[0]
-	const prefix = publish === 'github' ? '/ilsang-0gam' : ''
+  const publish = useState('github')[0]
+  const prefix = publish === 'github' ? '/ilsang-0gam' : ''
 
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path={`${prefix}/`} element={<Start />}></Route>
-				<Route path={`${prefix}/signup`} element={<SignUp />}></Route>
-				<Route path={`${prefix}/main`} element={<MainPage />}>
-					<Route path={`${prefix}/main/:id`} element={<Watch />} />
-				</Route>
-				<Route path={`${prefix}/write`} element={<Write />}></Route>
-				<Route path={`${prefix}/mypage`} element={<MyPage />}></Route>
-			</Routes>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={`${prefix}/`} element={<Start />}></Route>
+        <Route path={`${prefix}/signup`} element={<SignUp />}></Route>
+        <Route path={`${prefix}/main`} element={<MainPage />}></Route>
+        <Route path={`${prefix}/watch/:id`} element={<Watch />} />
+        <Route path={`${prefix}/write`} element={<Write />}></Route>
+        <Route path={`${prefix}/mypage`} element={<MyPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
