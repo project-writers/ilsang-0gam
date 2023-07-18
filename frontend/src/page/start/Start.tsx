4a3e44b49b771907ae4593c77228bfbe86bad7e9
wistaria <kogoome@kakao.com>
2023-07-18 22:12:18 +0900

    add google login2

--- frontend/src/page/start/Start.tsx
+++ frontend/src/page/start/Start.tsx
@@ -41,6 +41,7 @@ export function Start() {
         {/* 하단영역 */}
         <footer className="absolute bottom-0 left-0 w-full h-[276px] bg-white rounded-t-[40px] ">
           {/* 버튼 */}
+          구글 로그인 버튼 여기 
           <GoogleJWTBtn />
           <button
             className="relative top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 w-[90%] h-[60px] border-2 rounded-[80px] border-[#BABABA] text-center text-xl font-bold items-center "
