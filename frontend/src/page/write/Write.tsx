51d1eff266f9f9580431f7f8a16c8944460210c7
wistaria <kogoome@kakao.com>
2023-07-19 15:57:57 +0900

    fix error

--- frontend/src/page/write/Write.tsx
+++ frontend/src/page/write/Write.tsx
@@ -7,6 +7,7 @@ import ListItem from '@tiptap/extension-list-item'
 import TextStyle from '@tiptap/extension-text-style'
 
 export function Write() {
+  // const textStyleConfig = { types: [ListItem.name] }
   const editor = useEditor({
     editorProps: {
       attributes: {
@@ -15,7 +16,7 @@ export function Write() {
     },
     extensions: [
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
-      TextStyle.configure({ types: [ListItem.name] }),
+      // TextStyle.configure( textStyleConfig ),
       StarterKit.configure({
         bulletList: {
           keepMarks: true,
@@ -56,165 +57,146 @@ const MenuBar = ({ editor }: { editor: Editor | null }) => {
   const activeCss = 'border-2 border-zinc-500 px-1 rounded-md bg-zinc-500 text-white' 
   const nonActiveCss = 'border-2 border-zinc-300 px-1 rounded-md '
   return (
-    <>
-      <button
-        className={nonActiveCss}
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .undo()
-            .run()
-        }
-      >
-        undo
-      </button>
-      <button
-        className={nonActiveCss}
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .redo()
-            .run()
-        }
-      >
-        redo
-      </button>
-      mark |
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleBold()
-            .run()
-        }
-        className={editor.isActive('bold') ? activeCss : nonActiveCss}
-      >
-        <strong>b</strong>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleItalic()
-            .run()
-        }
-        className={editor.isActive('italic') ? activeCss : nonActiveCss}
-      >
-        <em>italic</em>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleStrike()
-            .run()
-        }
-        className={editor.isActive('strike') ? activeCss : nonActiveCss}
-      >
-        <strike>strike</strike>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={
-          !editor.can()
-            .chain()
-            .focus()
-            .toggleCode()
-            .run()
-        }
-        className={editor.isActive('code') ? activeCss : nonActiveCss}
-      >
-        ``
-      </button>
-      <button 
-        className={nonActiveCss}
-        onClick={() => editor.chain().focus().unsetAllMarks().run()}>
-        clear
-      </button>
-       node | 
-      <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={editor.isActive('paragraph') ? activeCss : nonActiveCss}
-      >
-        p
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive('heading', { level: 1 }) ? activeCss : nonActiveCss}
-      >
-        #
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive('heading', { level: 2 }) ? activeCss : nonActiveCss}
-      >
-        ##
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive('heading', { level: 3 }) ? activeCss : nonActiveCss}
-      >
-        ###
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive('bulletList') ? activeCss : nonActiveCss}
-      >
-        -
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive('orderedList') ? activeCss : nonActiveCss}
-      >
-        1.
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive('codeBlock') ? activeCss : nonActiveCss}
-      >
-        ```
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive('blockquote') ? activeCss : nonActiveCss}
-      >
-        &gt;
-      </button>
-      <button 
-        className={nonActiveCss}
-        onClick={() => editor.chain().focus().clearNodes().run()}>
-        clear
-      </button>
-      etc |
-      <button
-        onClick={() => editor.chain().focus().setHorizontalRule().run()}
-        className={nonActiveCss}
-      >
-        ---
-      </button>
-      <button
-        className={nonActiveCss}
-        onClick={() => editor.chain().focus().setHardBreak().run()}>
-        br
-      </button>
-      color |
-      <button
-        onClick={() => editor.chain().focus().setColor('#958DF1').run()}
-        className={editor.isActive('textStyle', { color: '#958DF1' }) ? activeCss : nonActiveCss + 'text-[#958DF1]'}
-      >
-        purple
-      </button>
-    </>
-  )
+		<>
+			<button
+				className={nonActiveCss}
+				onClick={() => editor.chain().focus().undo().run()}
+				disabled={!editor.can().chain().focus().undo().run()}
+			>
+				undo
+			</button>
+			<button
+				className={nonActiveCss}
+				onClick={() => editor.chain().focus().redo().run()}
+				disabled={!editor.can().chain().focus().redo().run()}
+			>
+				redo
+			</button>
+			mark |
+			<button
+				onClick={() => editor.chain().focus().toggleBold().run()}
+				disabled={!editor.can().chain().focus().toggleBold().run()}
+				className={editor.isActive('bold') ? activeCss : nonActiveCss}
+			>
+				<strong>b</strong>
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleItalic().run()}
+				disabled={!editor.can().chain().focus().toggleItalic().run()}
+				className={editor.isActive('italic') ? activeCss : nonActiveCss}
+			>
+				<em>italic</em>
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleStrike().run()}
+				disabled={!editor.can().chain().focus().toggleStrike().run()}
+				className={
+					editor.isActive('strike')
+						? activeCss + 'line-through'
+						: nonActiveCss + 'line-through'
+				}
+			>
+				strike
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleCode().run()}
+				disabled={!editor.can().chain().focus().toggleCode().run()}
+				className={editor.isActive('code') ? activeCss : nonActiveCss}
+			>
+				``
+			</button>
+			<button
+				className={nonActiveCss}
+				onClick={() => editor.chain().focus().unsetAllMarks().run()}
+			>
+				clear
+			</button>
+			node |
+			<button
+				onClick={() => editor.chain().focus().setParagraph().run()}
+				className={editor.isActive('paragraph') ? activeCss : nonActiveCss}
+			>
+				p
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+				className={
+					editor.isActive('heading', { level: 1 }) ? activeCss : nonActiveCss
+				}
+			>
+				#
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+				className={
+					editor.isActive('heading', { level: 2 }) ? activeCss : nonActiveCss
+				}
+			>
+				##
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+				className={
+					editor.isActive('heading', { level: 3 }) ? activeCss : nonActiveCss
+				}
+			>
+				###
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleBulletList().run()}
+				className={editor.isActive('bulletList') ? activeCss : nonActiveCss}
+			>
+				-
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleOrderedList().run()}
+				className={editor.isActive('orderedList') ? activeCss : nonActiveCss}
+			>
+				1.
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+				className={editor.isActive('codeBlock') ? activeCss : nonActiveCss}
+			>
+				```
+			</button>
+			<button
+				onClick={() => editor.chain().focus().toggleBlockquote().run()}
+				className={editor.isActive('blockquote') ? activeCss : nonActiveCss}
+			>
+				&gt;
+			</button>
+			<button
+				className={nonActiveCss}
+				onClick={() => editor.chain().focus().clearNodes().run()}
+			>
+				clear
+			</button>
+			etc |
+			<button
+				onClick={() => editor.chain().focus().setHorizontalRule().run()}
+				className={nonActiveCss}
+			>
+				---
+			</button>
+			<button
+				className={nonActiveCss}
+				onClick={() => editor.chain().focus().setHardBreak().run()}
+			>
+				br
+			</button>
+			color |
+			<button
+				onClick={() => editor.chain().focus().setColor('#958DF1').run()}
+				className={
+					editor.isActive('textStyle', { color: '#958DF1' })
+						? activeCss
+						: nonActiveCss + 'text-[#958DF1]'
+				}
+			>
+				purple
+			</button>
+		</>
+	)
 }
 
