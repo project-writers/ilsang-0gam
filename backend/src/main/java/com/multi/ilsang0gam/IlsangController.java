827ce7ef614170b80513aa12ebbfa997e72daba6
Starrain96 <bongohibari@gmail.com>
2023-08-05 15:03:19 +0900

    feat : backend

--- backend/src/main/java/com/multi/ilsang0gam/IlsangController.java
+++ backend/src/main/java/com/multi/ilsang0gam/IlsangController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,51 +13,48 @@ public class IlsangController {
 	@Autowired
 	IlsangDAO dao;
 	
-	@RequestMapping("insertBbs")
-	public int insert(IlsangVO bag) {
+	@RequestMapping("/insertBbs")
+	public void insert(IlsangVO bag) {
 		System.out.println("controller : " + bag);
 		dao.insert(bag);
-		return 1;
 	}
 
-	@RequestMapping("deleteBbs")
+	@RequestMapping("/deleteBbs")
 	public void delete(int ilsang_no) {
 		System.out.println("controller : " + ilsang_no);
 		dao.delete(ilsang_no);
 	}
 
-	@RequestMapping("updateBbs")
+	@RequestMapping("/updateBbs")
 	public void update(IlsangVO bag) {
 		System.out.println("controller : " + bag);
 		dao.update(bag);
 	}
 
-	@RequestMapping("onePenname")
+	@RequestMapping("/onePenname")
 	public void onePenname(String ilsang_penname, Model model) {
 		List<IlsangVO> list = dao.onePenname(ilsang_penname);
 		model.addAttribute("list", list);
-		System.out.println(list);
 	}
 	
-	@RequestMapping("oneTitle")
+	@RequestMapping("/oneTitle")
 	public void oneTitle(String ilsang_title, Model model) {
 		List<IlsangVO> list = dao.oneTitle(ilsang_title);
 		model.addAttribute("list", list);
-		System.out.println(list);
 	}
 	
-	@RequestMapping("oneTag")
+	@RequestMapping("/oneTag")
 	public void oneTag(String ilsang_tag, Model model) {
 		List<IlsangVO> list = dao.oneTag(ilsang_tag);
 		model.addAttribute("list", list);
-		System.out.println(list);
 	}
-
-	@RequestMapping("listBbs")
-	public void one(Model model) {
+	
+	
+	@RequestMapping("/listBbs")
+	public List<IlsangVO> one(Model model) {
 		List<IlsangVO> list = dao.list();
 		model.addAttribute("list", list);
-		System.out.println(list);
+		return list;
 	}
 
 }
